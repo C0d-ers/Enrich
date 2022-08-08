f086f123ed5ca909223de7a204fddd81afbdf7ff
C0d-ers <bjay.chaudhary.01@gmail.com>
2022-08-08 11:49:12 +0545

    Transactions

--- src/main/Executable/Others/data.java
+++ src/main/Executable/Others/data.java
@@ -14,19 +14,20 @@ public class data {
         data [5][0] = "Joyce Byers";        data [5][1] = "605010150053";
         data [6][0] = "Mike Wheeler";       data [6][1] = "605010150030";
         data [7][0] = "Dustin Henderson";   data [7][1] = "605010150034";
-        data [8][0] = "Nancy Wheeler";      data [8][1] = "605010150020";
+        data [8][0] = "Nancy Wheeler";      data [8][1] = "605010150020";  // 10k
         data [9][0] = "Will Byers";         data [9][1] = "605010150039";
 
         return data;
     }
     @DataProvider(name = "Left Contacts")
     public static Object [] [] leftContacts(){
-        Object[][] data = new Object[5][2];
+        Object[][] data = new Object[6][2];
         data [2][0] = "Mike Wheeler";       data [2][1] = "605010150030";
         data [3][0] = "Dustin Henderson";   data [3][1] = "605010150034";
         data [1][0] = "Nancy Wheeler";      data [1][1] = "605010150036";
-        data [0][0] = "Will Byers";         data [0][1] = "605010150039";
+        data [0][0] = "Will Byers";         data [0][1] = "605010150039";  //Loads of Saving Box
         data [4][0] = "Joyce Byers";        data [4][1] = "605010150053";
+        data [5][0] = "Bail Organa";        data [5][1] = "605010150028";
         return data;
     }
 
